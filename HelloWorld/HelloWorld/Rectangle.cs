058ddf7df22947a55f5b212ba001f533a7f5c4d3
chapagaisa <ankitchapagain@Ankits-Air.attlocal.net>
2019-07-20 12:05:20 -0500

    Copied the project to the git repo

---
+++ HelloWorld/HelloWorld/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Rectangle
+    {
+        //memeber variables
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 3.4;
+            width = 2.3;
+        }
+
+        public double GetArea()
+        {
+            double area;
+            area = length * width;
+            return area;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0|", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+
+        }
+
+
+    }
+
+
+}
